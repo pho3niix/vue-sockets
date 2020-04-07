503ee5f3f8e0b94844f14da0b61479f708c1683c
Hugo Costilla <hugo.costillah@gmail.com>
2020-04-06 23:08:06 -0500

    probando servidor

--- src/server/server.js
+++ src/server/server.js
@@ -31,8 +31,8 @@ app.use(cors());
 app.use(express.json());//procesar los datos en formato json
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));//static file for development
-// app.use(express.static(path.join(__dirname, '../../dist/index.html')));//static file for production
+app.use(express.static(path.join(__dirname,'../../dist')));
+app.use(express.static(path.join(__dirname,'../../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('dist'));
